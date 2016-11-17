300dae286887a73df147c76fe81f1d2538b7ea48
Aidan808 <buffblueboy@gmail.com>
2016-11-17 09:52:37 -1000

    will change later. These are the pages.

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var PAGE_0 = new CreatePage({
+	page_id : 3,
+	header : "You walk outside and see a path.",
+	main_text : "The path leads to an arena. You look around some more and find another path that leads to a church.",
+	left_text : "Go to arena",
+	left_page : "PAGE_ARENA",
+	right_text : "Go to church", 
+	right_page : "PAGE_CHURCH",
+	give_strength : 5,
+	give_items : ["Knife"]
+});
+var PAGE_1 = new CreatePage({
+	page_id : 3,
+	header : "You walk outside and see a path.",
+	main_text : "The path leads to an arena. You look around some more and find another path that leads to a church.",
+	left_text : "Go to arena",
+	left_page : "PAGE_ARENA",
+	right_text : "Go to church", 
+	right_page : "PAGE_CHURCH",
+	give_intel : 5,
+	give_items : ["Book"]
+});
